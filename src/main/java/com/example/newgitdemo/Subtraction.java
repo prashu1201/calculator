639642418b7907a60b80d47b0e69ed6c725fff90
amdocs <amdocs@gmail.com>
2019-07-10 12:18:03 +0530

    subtraction

---
+++ src/main/java/com/example/newgitdemo/Subtraction.java
@@ -0,0 +1,9 @@
+package com.example.newgitdemo;
+
+public class Subtraction {
+	public double subtraction( double firstNumber, double secondNumber)
+	{	
+		double result = (firstNumber - secondNumber);
+		return 	result;
+	}
+}
