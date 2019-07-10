7e1d464a24c58483815c5392ce883b0ec1d500ba
prashu1201 <52689943+prashu1201@users.noreply.github.com>
2019-07-10 12:06:36 +0530

    Delete multiplication.java
    comment properly

--- src/main/java/com/example/newgitdemo/multiplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.newgitdemo;
-
-public class multiplication {
-	public double muliplication(double firstoperand,double secondoperand)
-	{
-		return (firstoperand*secondoperand);
-	}
-
-}
